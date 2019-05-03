515b7a7df708081b08c385c4450e8a3d71a5b937
cankush625 <cankush625@gmail.com>
2019-05-03 11:28:43 +0530

    Detect closest pair to the target value and detect cycles in the linked list programs added

---
+++ Hackerrank Challenges/CyclesInLinkedList.java
@@ -0,0 +1,58 @@
+//Detect a cycle in the linked list. Note that the head pointer may be 'null' if the list is empty.
+
+package LinkedList;
+import java.util.Scanner;
+
+public class CyclesInLinkedList {
+    private static Scanner scanner = new Scanner(System.in);
+    static class node{
+        int data;
+        node next;
+        node prev;
+
+        public node(int data){
+            this.data = data;
+        }
+    }
+
+    static boolean hasCycles(node head){
+        if (head == null){
+            return false;
+        }
+
+        node fast = head.next;
+        node slow = head;
+        while(fast != null && fast.next != null && slow != null){
+            if (fast == slow){
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        node head = new node(2);
+        node nodeB = new node(3);
+        node nodeC = new node(5);
+        node nodeD = new node(7);
+
+        head.next = nodeB;
+        nodeB.next = nodeC;
+        nodeB.prev = head;
+        nodeC.next = nodeD;
+        nodeC.prev = nodeB;
+        nodeD.next = head;
+        nodeD.prev = nodeC;
+
+        System.out.println(hasCycles(head));
+    }
+}
+
+// by Ankush Chavan
+
+/*
+Output :
+    true
+*/

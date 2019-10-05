f42e51d91b5560ce8584355eefca6ebb4ea3758a
cankush625 <cankush625@gmail.com>
2019-10-05 21:49:51 +0530

    Code enhancedv and string validated

--- InterviewQues/syntax_checking/SyntaxChecker.java
+++ InterviewQues/syntax_checking/SyntaxChecker.java
@@ -5,14 +5,34 @@ import java.util.Stack;
 public class SyntaxChecker {
 	public boolean syntaxChecker(String array) {
 		Stack<Character> stack = new Stack<Character>();
+		int one = 0;
+		int two = 0;
+		int three = 0;
+		int four = 0;
 		
 		for(int i = 0; i < array.length(); i++) {
-			if(array.charAt(i) == '<' || array.charAt(i) == '[') {
-				stack.push(array.charAt(i));
+			if(array.charAt(i) == '<') {
+				one++;
+			} else if(array.charAt(i) == '>') {
+				two++;
+			} else if(array.charAt(i) == '[') {
+				three++;
+			} else if(array.charAt(i) == ']') {
+				four++;
+			} else {
+				return false;
 			}
-			
-			if(array.charAt(i) == '>' || array.charAt(i) == ']') {
-				stack.pop();
+		}
+		
+		if (one == two && three == four) {
+			for(int i = 0; i < array.length(); i++) {
+				if(array.charAt(i) == '<' || array.charAt(i) == '[') {
+					stack.push(array.charAt(i));
+				}
+				
+				if(array.charAt(i) == '>' || array.charAt(i) == ']') {
+					stack.pop();
+				}
 			}
 		}
 		

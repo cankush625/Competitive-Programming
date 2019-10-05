0ef49cd1add020845f006cf0dcb745b3882541cf
cankush625 <cankush625@gmail.com>
2019-10-05 23:39:11 +0530

    Code updated and optimized solution is added

--- InterviewQues/syntax_checking/SyntaxChecker.java
+++ InterviewQues/syntax_checking/SyntaxChecker.java
@@ -5,37 +5,17 @@ import java.util.Stack;
 public class SyntaxChecker {
 	public boolean syntaxChecker(String array) {
 		Stack<Character> stack = new Stack<Character>();
-		int one = 0;
-		int two = 0;
-		int three = 0;
-		int four = 0;
 		
 		for(int i = 0; i < array.length(); i++) {
-			if(array.charAt(i) == '<') {
-				one++;
-			} else if(array.charAt(i) == '>') {
-				two++;
-			} else if(array.charAt(i) == '[') {
-				three++;
-			} else if(array.charAt(i) == ']') {
-				four++;
+			if(array.charAt(i) == '<' || array.charAt(i) == '[') {
+				stack.push(array.charAt(i));
 			} else {
-				return false;
-			}
-		}
-		
-		if (one == two && three == four) {
-			for(int i = 0; i < array.length(); i++) {
-				if(array.charAt(i) == '<' || array.charAt(i) == '[') {
+				if((array.charAt(i) == '>' && stack.peek() == '<') || (array.charAt(i) == ']' && stack.peek() == '[')) {
+					stack.pop();
+				} else {
 					stack.push(array.charAt(i));
 				}
-				
-				if(array.charAt(i) == '>' || array.charAt(i) == ']') {
-					stack.pop();
-				}
 			}
-		} else {
-			return false;
 		}
 		
 		return stack.empty();

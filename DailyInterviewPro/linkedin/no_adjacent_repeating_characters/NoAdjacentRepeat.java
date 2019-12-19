060f9fbdfaa711b84cce882cb1c2a13abcd05f26
cankush625 <cankush625@gmail.com>
2019-12-19 19:48:56 +0530

    Fixed - Solves any string if possible
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- DailyInterviewPro/linkedin/no_adjacent_repeating_characters/NoAdjacentRepeat.java
+++ DailyInterviewPro/linkedin/no_adjacent_repeating_characters/NoAdjacentRepeat.java
@@ -10,6 +10,7 @@ public class NoAdjacentRepeat {
 		int maxVal = 0;
 		
 		// Copying characters from string to array
+		// Also getting the max occured character
 		for (int i = 0; i < input.length(); i++) {
 			inputArray[i] = input.charAt(i);
 			if (max.containsKey(inputArray[i])) {
@@ -24,23 +25,15 @@ public class NoAdjacentRepeat {
 				}
 			}
 		}
-		System.out.println(maxVal);
-		while ((maxVal/2) >= 0) {
+		while (Math.floorDiv(maxVal, 2) >= 0) {
 			for (int i = 0; i < inputArray.length; i++) {
 				// If the current element and the previous element are same then swapping the current element and next element
 				// This will solves out problem except for the last element if the last and second last elements are same
 				try {
-					System.out.println((inputArray[i] != inputArray[i + 1]));
-					if ((inputArray[i-1] == inputArray[i]) && (i > 0) && (inputArray[i] != inputArray[i + 1])) {
+					if ((inputArray[i-1] == inputArray[i]) && (i > 0)) {
 						char tempChar = inputArray[i];
 						inputArray[i] = inputArray[i + 1];
 						inputArray[i + 1] = tempChar;
-					} else if (inputArray[i-1] == inputArray[i] && inputArray[i] == inputArray[i + 1]) {
-						if (inputArray[i] != inputArray[i + 2]) {
-							char tempChar = inputArray[i];
-							inputArray[i] = inputArray[i + 2];
-							inputArray[i + 2] = tempChar;
-						}
 					}
 				} catch(Exception e) {
 					System.out.println(e.getMessage());

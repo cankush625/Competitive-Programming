bb672233f768dcbe6d0759f27641510688301d18
cankush625 <cankush625@gmail.com>
2019-12-19 19:39:49 +0530

    Fixed - Solves any string if possible
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- DailyInterviewPro/linkedin/no_adjacent_repeating_characters/NoAdjacentRepeat.java
+++ DailyInterviewPro/linkedin/no_adjacent_repeating_characters/NoAdjacentRepeat.java
@@ -1,35 +1,61 @@
 package linkedin.no_adjacent_repeating_characters;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NoAdjacentRepeat {
 	public void noAdjacentRepeat(String input) {
 		char[] inputArray = new char[input.length()];
+		Map<Character, Integer> max = new HashMap<Character, Integer>();
+		int maxVal = 0;
 		
 		// Copying characters from string to array
 		for (int i = 0; i < input.length(); i++) {
 			inputArray[i] = input.charAt(i);
+			if (max.containsKey(inputArray[i])) {
+				max.put(inputArray[i], max.get(inputArray[i])+1);
+				if (maxVal < max.get(inputArray[i])) {
+					maxVal = max.get(inputArray[i]);
+				}
+			} else {
+				max.put(inputArray[i], 1);
+				if (maxVal < max.get(inputArray[i])) {
+					maxVal = max.get(inputArray[i]);
+				}
+			}
 		}
-		
-		for (int i = 0; i < inputArray.length; i++) {
-			// If the current element and the previous element are same then swapping the current element and next element
-			// This will solves out problem except for the last element if the last and second last elements are same
-			try {
-				if ((inputArray[i-1] == inputArray[i]) && (i > 0)) {
-					char tempChar = inputArray[i];
-					inputArray[i] = inputArray[i + 1];
-					inputArray[i + 1] = tempChar;
+		System.out.println(maxVal);
+		while ((maxVal/2) >= 0) {
+			for (int i = 0; i < inputArray.length; i++) {
+				// If the current element and the previous element are same then swapping the current element and next element
+				// This will solves out problem except for the last element if the last and second last elements are same
+				try {
+					System.out.println((inputArray[i] != inputArray[i + 1]));
+					if ((inputArray[i-1] == inputArray[i]) && (i > 0) && (inputArray[i] != inputArray[i + 1])) {
+						char tempChar = inputArray[i];
+						inputArray[i] = inputArray[i + 1];
+						inputArray[i + 1] = tempChar;
+					} else if (inputArray[i-1] == inputArray[i] && inputArray[i] == inputArray[i + 1]) {
+						if (inputArray[i] != inputArray[i + 2]) {
+							char tempChar = inputArray[i];
+							inputArray[i] = inputArray[i + 2];
+							inputArray[i + 2] = tempChar;
+						}
+					}
+				} catch(Exception e) {
+					System.out.println(e.getMessage());
 				}
-			} catch(Exception e) {
-				System.out.println(e.getMessage());
-			}
-			// For last element, checking if the last element and first element are same.
-			// If last element and first element are not and last element and second last element are same then swapping the last and first element
-			if (i == inputArray.length - 1) {
-				if (inputArray[i] == inputArray[i - 1] && inputArray[i] != inputArray[0]) {
-					char tempChar = inputArray[i];
-					inputArray[i] = inputArray[0];
-					inputArray[0] = tempChar;
+				// For last element, checking if the last element and first element are same.
+				// If last element and first element are not and last element and second last element are same then swapping the last and first element
+				if (i == inputArray.length - 1) {
+					if (inputArray[i] == inputArray[i - 1] && inputArray[i] != inputArray[0]) {
+						char tempChar = inputArray[i];
+						inputArray[i] = inputArray[0];
+						inputArray[0] = tempChar;
+					}
 				}
 			}
+			maxVal--;
 		}
 		System.out.println("output:");
 		System.out.println(inputArray);
--- DailyInterviewPro/linkedin/no_adjacent_repeating_characters/NoAdjacentRepeatMain.java
+++ DailyInterviewPro/linkedin/no_adjacent_repeating_characters/NoAdjacentRepeatMain.java
@@ -4,7 +4,9 @@ public class NoAdjacentRepeatMain {
 	public static void main(String[] args) {
 		NoAdjacentRepeat noAdjRpt = new NoAdjacentRepeat();
 		
-		String input = "abbccc";
+//		String input = "abbccc";
+//		String input = "abbacccbb";
+		String input = "aaaabbaabcccc";
 		
 		noAdjRpt.noAdjacentRepeat(input);
 	}

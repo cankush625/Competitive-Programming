a17e34ed6d3aaf7cf542e74ea5792592d27c7b5a
Ankush Chavan <41515472+cankush625@users.noreply.github.com>
2020-07-02 19:39:41 +0530

    July Leetcoding Challenge (#10)
    * Leetcode problem 11 solution
    
    Signed-off-by: cankush625 <cankush625@gmail.com>
    
    * Rotate image coding blocks challenge solution
    
    Signed-off-by: cankush625 <cankush625@gmail.com>
    
    * Solution for prime visit problem on coding blocks
    
    Signed-off-by: cankush625 <cankush625@gmail.com>
    
    * Code for gcd problem is added
    
    Signed-off-by: cankush625 <cankush625@gmail.com>
    
    * Solution for counting trailing zeroes in number
    
    Signed-off-by: cankush625 <cankush625@gmail.com>
    
    * Soluiton for Google KickStart 2020 allocation problem
    
    Signed-off-by: cankush625 <cankush625@gmail.com>
    
    * July Leetcoding challenge problem 1
    
    Signed-off-by: cankush625 <cankush625@gmail.com>
    
    * July Leetcoding Challenge day1 - Unwanted check removed
    
    Signed-off-by: cankush625 <cankush625@gmail.com>
    
    * July LeetCoding Challenge day2
    
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ LeetcodeChallenges/july_leetcoding_challenge/day1/Solution.java
@@ -0,0 +1,20 @@
+package july_leetcoding_challenge.day1;
+
+public class Solution {
+	public int arrangeCoins(int n) {
+        int count = 0;
+        int row = 1;
+    	while (n - row >= 0) {
+			n = n - row;
+			count++;
+			row++;
+    	}
+        return count;
+    }
+	
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int n = 8;
+		System.out.println(solution.arrangeCoins(n));
+	}
+}

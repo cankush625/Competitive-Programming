e48f5840a69bd462d99f271d06116ad7c447b446
cankush625 <cankush625@gmail.com>
2020-07-27 16:00:25 +0530

    Leetcode problem 88 solution enhanced
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- Leetcode/cp88_merge_sorted_array/Solution.java
+++ Leetcode/cp88_merge_sorted_array/Solution.java
@@ -1,31 +1,14 @@
 package cp88_merge_sorted_array;
-import java.util.Collections;
+
+import java.util.Arrays;
 
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(nums1 == null || nums2 == null) {
-        	return;
-        } else {
-        	int j = 0;
-        	for (int i = m; i < m+n; i++) {
-        		nums1[i] = nums2[j];
-        		j++;
-        	}
-        	
-        	boolean isSorted = false;
-        	while (!isSorted) {
-        		isSorted = true;
-	        	for (int i = 0; i < nums1.length - 1; i++) {
-	        		int k = i + 1;
-	        			if (nums1[i] > nums1[k]) {
-		        			int temp = nums1[i];
-		        			nums1[i] = nums1[k];
-		        			nums1[k] = temp;
-		        			isSorted = false;
-	        			}
-	        	}
-        	}
-        	System.out.println(nums1.toString());
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j = 0;
+        for (int i = m; i < nums1.length; i++) {
+                nums1[i] = nums2[j];
+            j++;
         }
+        Arrays.sort(nums1);
     }
 }

be47e3ae5a73aedfd604cd57429ea21baff7adce
cankush625 <cankush625@gmail.com>
2020-08-31 23:08:11 +0530

    Codechef August Challenege 2020 CRDGAME3 solution updated
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- CODECHEF/August Challenege 2020/CRDGAME3.cpp
+++ CODECHEF/August Challenege 2020/CRDGAME3.cpp
@@ -9,12 +9,22 @@ int main() {
         int powerOfChef, powerOfRick;
         cin >> powerOfChef >> powerOfRick;
         int noOfDigitsForChef;
+        int noOfDigitsForRick;
         if (powerOfChef % 9 == 0) {
             noOfDigitsForChef = powerOfChef / 9;
         } else {
             noOfDigitsForChef = (powerOfChef / 9) + 1;
         }
-        
+        if (powerOfRick % 9 == 0) {
+            noOfDigitsForRick = powerOfRick / 9;
+        } else {
+            noOfDigitsForRick = (powerOfRick / 9) + 1;
+        }
+        if (noOfDigitsForChef == noOfDigitsForRick || noOfDigitsForRick < noOfDigitsForChef) {
+            cout << "1 " << noOfDigitsForRick << endl;
+        } else {
+            cout << "0 " << noOfDigitsForChef << endl;
+        }
         noOfTestCases--;
     }
 	return 0;

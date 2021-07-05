5e46e7e7bdeed9fe57b0e7db1c8486c2cc4c5a92
cankush625 <cankush625@gmail.com>
2021-07-05 22:18:08 +0530

    Leetcode July Challenge Day 5: Reshape the Matrix
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ LeetcodeChallenges/july_leetcode_challenge_2021/day5/Solution.java
@@ -0,0 +1,22 @@
+// Reshape the Matrix
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        if (r*c != mat.length * mat[0].length) {
+            return mat;
+        }
+        int[][] res = new int[r][c];
+        int row = 0;
+        int col = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                res[row][col] = mat[i][j];
+                col++;
+                if (col == c) {
+                    row++;
+                    col = 0;
+                }
+            }
+        }
+        return res;
+    }
+}

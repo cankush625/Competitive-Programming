29a257277d91d3bd667d18161061e965cbe590a1
cankush625 <cankush625@gmail.com>
2021-07-06 10:22:03 +0530

    Leetcode 5: Longest Palindromic Substring - Easy Solution
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- Leetcode/cp5_longest_palindromic_substring/Solution.java
+++ Leetcode/cp5_longest_palindromic_substring/Solution.java
@@ -1,53 +1,29 @@
-package cp5_longest_palindromic_substring;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Solution {
-	public String longestPalindrome(String s) {
-		List<String> palindromes = new ArrayList<String>();
-		int max = 0;
-		String maxString = "";
-		
-        for (int i = 0; i < s.length(); i++) {
-        	StringBuilder sb = new StringBuilder();
-        	for (int j = i; j < s.length(); j++) {
-        		sb.append(s.charAt(j));
-        		if (isPalindrome(sb.toString())) {
-        			palindromes.add(sb.toString());
-        		}
-        	}
+class Solution {
+    private int start = 0;
+    private int maxLen = 0;
+    
+    public String longestPalindrome(String s) {
+        if (s.length() < 2) {
+            return s;
         }
         
-        for (int i = 0; i < palindromes.size(); i++) {
-        	if (palindromes.get(i).length() > max) {
-        		max = palindromes.get(i).length();
-        		maxString = palindromes.get(i);
-        	}
+        // Try to extend palindrome as much as possible from current character
+        for (int i = 0; i < s.length(); i++) {
+            extendPalindrome(s, i, i); // assume odd length
+            extendPalindrome(s, i, i+1); // assume even length
         }
-        return maxString;
+        return s.substring(start, start + maxLen);
     }
-	
-	public static boolean isPalindrome(String str) {
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = str.length()-1; i >= 0; i--) {
-			sb.append(str.charAt(i));
-		}
-		
-		String palStr = sb.toString();
-		if (palStr.equals(str)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public static void main(String[] args) {
-		Solution sol = new Solution();
-		
-		String str = "";
-		
-		System.out.println(sol.longestPalindrome(str));		
-	}
+    
+    private void extendPalindrome(String word, int low, int high) {
+        while (low >= 0 && high < word.length() && word.charAt(low) == word.charAt(high)) {
+            low--;
+            high++;
+        }
+        // For maxlen, we are doing -1 because the value of low is one minus the actual index of start of palindrome and value of high is one plus the actual index of end of palindrome
+        if (maxLen < high - low - 1) { 
+            start = low + 1;
+            maxLen = high - low - 1;
+        }
+    }
 }

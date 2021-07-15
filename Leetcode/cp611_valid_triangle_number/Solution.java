5adb2e74e263745a14c4632976e59fe03db18425
cankush625 <cankush625@gmail.com>
2021-07-15 22:39:31 +0530

    Leetcode 611: Valid triangle number
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ Leetcode/cp611_valid_triangle_number/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    // Sort the array in ascending order and start looking for numbers from the last element(largest number) in sorted array 
+    // to the first number.
+    public int triangleNumber(int[] nums) {
+        int answer = 0;
+        int first = nums.length - 1;
+        Arrays.sort(nums);
+        while (first > 1) {
+            int firstNum = nums[first];
+            int start = 0;
+            int end = first - 1;
+            
+	    // Three numbers p, q, r are triangle numbers if p + q > r for all combinations of p, q, r
+            while (start < end) {
+                if (nums[start] + nums[end] > firstNum) {
+                    answer += (end - start);
+                    end--;
+                } else {
+                    start++;
+                }
+            }
+            first--;
+        }
+        return answer;
+    }
+}

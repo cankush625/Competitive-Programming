e95ab889f5402aabaf5b4f9fcb335ed3cae7afef
cankush625 <cankush625@gmail.com>
2021-08-23 22:39:55 +0530

    Leetcode 653: Two sum IV Input is BST
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- Leetcode/cp653_two_sum_IV_input_is_bst/Solution.java
+++ Leetcode/cp653_two_sum_IV_input_is_bst/Solution.java
@@ -1,10 +1,10 @@
 class Solution {
-    // Time complexity O(n*logn)
+    // Time complexity O(n) where n = number of nodes in BST
     // Space complexity O(n) where n = number of nodes in BST
     public boolean findTarget(TreeNode root, int k) {
         List<Integer> arrayList = new ArrayList<Integer>();
+        // Inorder traversal of BST gives elements in sorted order
         getNodes(root, arrayList);
-        Collections.sort(arrayList);
         int start = 0;
         int end = arrayList.size() - 1;
         while (start < end) {

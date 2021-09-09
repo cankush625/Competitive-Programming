34f994f5353f86d7ae9023078530cd0254246b09
cankush625 <cankush625@gmail.com>
2021-09-10 00:12:35 +0530

    Leetcode 83: Remove duplicates from sorted list
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ Leetcode/cp83_remove_duplicates_from_sorted_list/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode temp = head;
+        while (temp.next != null) {
+            if (temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}

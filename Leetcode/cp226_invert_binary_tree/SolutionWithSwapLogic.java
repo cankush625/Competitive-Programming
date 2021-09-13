736dd5d955d5a74f14c6d0e3ec2a9924a8413dde
cankush625 <cankush625@gmail.com>
2021-09-13 22:26:21 +0530

    Leetcode 226: Invert binary Tree
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ Leetcode/cp226_invert_binary_tree/SolutionWithSwapLogic.java
@@ -0,0 +1,13 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}

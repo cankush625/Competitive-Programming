b74c9be3442695c17746f6972f4bedb5ddaaa22b
cankush625 <cankush625@gmail.com>
2021-09-17 22:49:44 +0530

    Leetcode 75: Sort colors [MEDIUM]
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ Leetcode/cp75_sort_colors/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Using DNF sort
+    public void sortColors(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int i = 0;
+        while (i <= end) {
+            if (nums[i] == 0) {
+                int temp = nums[start];
+                nums[start] = nums[i];
+                nums[i] = temp;
+                start++;
+                i++;
+            } else if (nums[i] == 2) {
+                int temp = nums[end];
+                nums[end] = nums[i];
+                nums[i] = temp;
+                end--;
+            } else {
+                i++;
+            }
+        }
+    }
+}

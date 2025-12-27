7e0c12cd426b42b752016109297ab55383eefc77
Ankush Chavan <cankush625@gmail.com>
2025-12-27 22:23:25 +0530

    feat(Leetcode 242): Valid anagram [EASY]
    Signed-off-by: Ankush Chavan <cankush625@gmail.com>

---
+++ Leetcode/cp242_valid_anagram/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    # Leetcode Problem: https://leetcode.com/problems/valid-anagram/
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Generate the char count map for s
+        s_char_count_map = {}
+        for char in s:
+            s_char_count_map[char] = s_char_count_map.get(char, 0) + 1
+        # Generate the char count map for t
+        t_char_count_map = {}
+        for char in t:
+            t_char_count_map[char] = t_char_count_map.get(char, 0) + 1
+        # If length of unique chars in s and t is not same then return False
+        if len(s_char_count_map) != len(t_char_count_map):
+            return False
+        for k, v in s_char_count_map.items():
+            # If the count of any character doesn't match then return False
+            if v != t_char_count_map.get(k):
+                return False
+        return True
